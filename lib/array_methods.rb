719665177d2daf9b054e2bb09c1c53e2d5d5773c
Maverick Adolfo <merkonical@gmail.com>
2019-08-10 04:46:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,7 +6,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.max { |a, b| array.length <=> b.length }
+  a.max { |a, b| a.length <=> b.length }
 end
 
 def find_min_value(array)
